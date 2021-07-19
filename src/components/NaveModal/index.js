255da97231dca23da4501ebc7a2b77d1789a546b
Marks-Oliveira <marcosroliveira@outlook.com>
2021-07-19 03:12:39 -0300

    Alterado a função 'goToEditNavePage' do arquivo NaveModal

--- src/components/NaveModal/index.js
+++ src/components/NaveModal/index.js
@@ -1,21 +1,39 @@
 import React from 'react';
 import moment from 'moment';
 import { useHistory } from 'react-router';
+import { toast } from 'react-toastify';
+
+import api from '../../services/api';
+import { useUserIssues } from '../../providers/userContext';
 
 import * as S from './styles';
 import DeleteSharpIcon from '@material-ui/icons/DeleteSharp';
 import EditSharpIcon from '@material-ui/icons/EditSharp';
 
 const NaveModal = ({ item, displayModal, displayModalDeleteNave }) => {
+    const { setUser } = useUserIssues();
     const history = useHistory()
 
     const yearsInTheCompany = moment().diff(item.admission_date, 'years');
     const monthsInTheCompany = moment().diff(item.admission_date, 'months');
     const months = monthsInTheCompany % 12;
 
-    const goToEditNavePage = () => {
-        history.push(`/navers/edit/${item.id}`);
-        displayModal();
+    const goToEditNavePage = async () => {
+        try {
+            const config = {
+                headers: {
+                  Authorization: `Bearer ${window.localStorage.getItem('token')}`,
+                },
+            };
+
+            const response = await api.get(`/navers/${item.id}`, config);
+
+            setUser(response.data);
+            displayModal();
+            history.push(`/navers/edit/${item.id}`);
+        } catch (error) {
+            toast.error("Erro ao carregar usuário naver");
+        }
     };
 
     return (
